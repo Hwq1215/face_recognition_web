31ddead55a4653328200db4134148bd45d04249a
hwq <1305934019@qq.com>
2023-03-27 20:57:24 +0800

    new-ui

---
+++ front/ui_improve/src/modules/table/views/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="w-full block mx-auto h-auto py-2">
+    <div class="flex flex-wrap flex-col bg-white shadow mb-7 mx-auto rounded-md">
+      <div class="py-5 px-6 border-b border-primary-white">
+        <h3 class="cursor-auto">人脸信息</h3>
+      </div>
+      <div class="block overflow-x-auto w-full">
+        <ProjectTable :facesData="facesData" :pageSize="page-size" />
+      </div>
+      <!-- <div class="p-4">
+        <el-pagination 
+        :page-size="pageSize"
+        :pager-count="5"
+        v-model:current-page="currentPage"
+        background
+        layout="prev, pager, next"
+        :total="total"
+        class="mt-4"
+        />
+      </div> -->
+    </div>
+    <!-- <div class="flex flex-wrap flex-col shadow mb-7 mx-auto rounded-md bg-dark">
+      <div class="py-5 px-6 border-b border-dark-light">
+        <h3 class="cursor-auto text-slate-50">Dark table</h3>
+      </div>
+      <div class="block overflow-x-auto w-full">
+        <ProjectTable :tableData="tableData" :theme="'dark'" />
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import {getFacesData} from '@/api/getData'
+import John_Snow_Url from '@/assets/images/John_Snow.png'
+import Team_2_Url from '@/assets/images/team-2.jpg'
+import Team_3_Url from '@/assets/images/team-3.jpg'
+import Team_4_Url from '@/assets/images/team-4.jpg'
+import Logo_Url from '@/assets/images/ltv_logo.png'
+import ProjectTable from './components/ProjectTable.vue'
+
+export default {
+  name: 'Tables',
+  data(){
+    return {
+      facesData: [],
+      pageSize: 13,
+      currentPage: 1,
+      total: 0,
+    }
+  },
+  
+  methods: {
+    init(){
+      this.ProjectTable.init()
+    }
+  },
+  components: {
+    ProjectTable,
+  },
+}
+</script>
---
+++ front/ui_improve/src/myViews/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="body">
+            <Header></Header>
+                <router-view />
+            <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+    name: 'HomeView',    
+    data() {
+        return{
+        }
+    },
+    components: {
+        Header,
+        Footer,
+    }
+}
+</script>
+
+<style>
+.body{
+    width: 100%;
+    height: 100%;
+}
+</style>
